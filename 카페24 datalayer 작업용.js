2aaf444ef77eda3b754f0fdae6fb6bfcd2255b44
chickenbeerman <sunwoo9891@naver.com>
2021-01-04 18:46:48 +0900

    commit

--- 카페24 datalayer 작업용.js
+++ 카페24 datalayer 작업용.js
@@ -26,37 +26,64 @@ function() {
 // funnel 2 variable - 장바구니 추가
 
 
-
-
 // funnel 2 tag - 장바구니 추가
-<script>
-    // 장바구니에 담기 버튼 클릭시 이벤트    
-    function addtoCart(){
-        // 수량이 1이상이어야 장바구니에 담기 이벤트가 실행됨
-        var total_quantity = $(".total").html().substring($(".total").html().indexOf('(')+1, $(".total").html().indexOf('개'));
-
-        if( total_quantity && total_quantity > 0){
-
-            dataLayer.push({
-                'event': 'addToCart',
-                'ecommerce': {
-                    'currencyCode': 'KRW',
-                    'add': {                                // 'add' actionFieldObject measures.
-                        'products': [{                    //  adding a product to a shopping cart.
-                            'name': '{$name}',          // Name or ID is required.
-                            'id': '{$product_no}',
-                            'price': '{$product_sale_price}'.split('.')[0],
-                            'brand': 'Healo',
-                            'category': '{$cate_no}',
-                            'quantity': total_quantity
-                        }]
-                    }
-                }
-            });
+// 장바구니에 담기 버튼 클릭 시 트리거
+    <script>
+      dataLayer.push({
+        'event': 'addToCart',
+        'ecommerce': {
+          'detail': {
+            'actionField': {'step': 2},
+            'products': {{funnel_1_제품조회_배열}}
+          }
         }
+      });
+    </script>
+
+
+// funnel 3 variable - 결제시작 주문서 작성
+function() {
+  var source = aBasketProductOrderData;
+  var productInfo = [];
+  for(var i = 0 ; i < source.length ; i++) {
+      var productId = source[i].product_no;
+      var productPrice = source[i].product_sale_price;
+      var productQty = source[i].quantity;
+      var productCate = source[i].main_cate_no;
+
+      productInfo.push({
+        'id': productId,
+        'price': productPrice,
+        'quantity': productQty,
+        'category': productCate
+      });
   }
+  return productInfo;
+}
+
+// funnel 3 tag - 결제시작 주문서 작성
+<script>
+  dataLayer.push({
+    'event': 'checkout',
+    'ecommerce': {
+    'checkout': {
+      'actionField': {'step': 3},
+      'products': {{funnel_3_결제시작_배열}}
+      }
+    }
+  });
 </script>
 
+// funnel 4 variable - 주문완료 - transaction id
+function() {
+  var source = EC_FRONT_EXTERNAL_SCRIPT_VARIABLE_DATA;
+  var orderId = source.order_id;
+	return orderId;
+}
 
-
-#quantity
+// funnel 4 variable - 주문완료 - 배송비
+function() {
+  var source = EC_FRONT_EXTERNAL_SCRIPT_VARIABLE_DATA;
+  var shippingFee = source.total_basic_ship_fee;
+	return shippingFee;
+}
